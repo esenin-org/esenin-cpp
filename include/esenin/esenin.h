e17f3abbdbeadbb1d99bf2bc958e2686569143a6
vovapolu <vovapolu@gmail.com>
2019-04-07 20:39:41 +0300

    Add typed responses, support for all esenin methods

--- include/esenin/esenin.h
+++ include/esenin/esenin.h
@@ -4,6 +4,24 @@
 #include <string>
 #include <vector>
 
+struct PosWord {
+    const std::string word;
+    const std::string connection_label;
+    const int connection_index;
+    const std::string pos;
+
+    PosWord(
+        const std::string &word, 
+        const std::string &connection_label, 
+        int connection_index,
+        const std::string &pos)
+        : word(word), 
+          connection_label(connection_label), 
+          connection_index(connection_index), 
+          pos(pos) 
+    {}
+};
+
 class Client {
 
 private:
@@ -12,9 +30,9 @@ private:
 
 public:
     Client(std::string const& ip, long port);
-    std::string get_pos(std::string const& text);
-    // std::string fit_topics(std::vector<std::vector<std::string>> const& terms, int topics);
-    // std::string get_topics(std::string const& id, std::string const& term);
+    std::vector<PosWord> get_pos(std::string const& text);
+    std::string fit_topics(std::vector<std::vector<std::string>> const& terms, int topics);
+    std::vector<double> get_topics(std::string const& id, std::string const& term);
 };
 
 #endif
--- src/esenin.cxx
+++ src/esenin.cxx
@@ -55,19 +55,96 @@ Client::Client(std::string const& ip, long port)
   this->port = port;
 }
 
-std::string Client::get_pos(std::string const& text)
+std::vector<PosWord> Client::get_pos(std::string const& text)
 {
-  rapidjson::Value json_val;
   rapidjson::Document doc;
   auto& allocator = doc.GetAllocator();
 
   doc.SetObject();
-  json_val.SetString(text.c_str(), allocator);
-  doc.AddMember("string", json_val, allocator);
+  doc.AddMember("text", rapidjson::Value(text.c_str(), allocator).Move(), allocator);
 
-  return call_esenin_server(
+  std::string response = call_esenin_server(
     "http://" + this->ip + "/nlp/pos",
     this->port, 
     doc
   );
+
+  rapidjson::Document result_doc;
+
+  result_doc.Parse(response.c_str());
+  const rapidjson::Value& value_words = result_doc["words"];
+
+  std::vector<PosWord> words;
+  words.reserve(value_words.Size());
+  for (rapidjson::SizeType i = 0; i < value_words.Size(); i++) {
+    const rapidjson::Value& value_word = value_words[i];
+    words.emplace_back(PosWord(
+      std::string(value_word["word"].GetString()), 
+      std::string(value_word["connection_label"].GetString()),
+      value_word["connection_index"].GetInt(),
+      std::string(value_word["pos"].GetString())
+    ));
+  }
+
+  return words;
+}
+
+std::string Client::fit_topics(std::vector<std::vector<std::string>> const& terms, int topics)
+{
+  rapidjson::Value terms_val;
+  rapidjson::Document doc;
+  auto& allocator = doc.GetAllocator();
+
+  doc.SetObject();
+  terms_val.SetArray();
+  for (size_t i = 0; i < terms.size(); i++) {
+    rapidjson::Value nested_terms_val;
+    nested_terms_val.SetArray();
+    for (size_t j = 0; j < terms[i].size(); j++) {
+      nested_terms_val.PushBack(rapidjson::Value(terms[i][j].c_str(), allocator), allocator);
+    }
+    terms_val.PushBack(nested_terms_val, allocator);
+  }
+  doc.AddMember("terms", terms_val, allocator);
+  doc.AddMember("topics", topics, allocator);
+
+  std::string response = call_esenin_server(
+    "http://" + this->ip + "/nlp/tm/fit",
+    this->port, 
+    doc
+  );
+
+  rapidjson::Document result_doc;
+
+  result_doc.Parse(response.c_str());
+  return result_doc["id"].GetString();
+}
+
+std::vector<double> Client::get_topics(std::string const& id, std::string const& term)
+{
+  rapidjson::Document doc;
+  auto& allocator = doc.GetAllocator();
+
+  doc.SetObject();
+  doc.AddMember("id", rapidjson::Value(id.c_str(), allocator), allocator);
+  doc.AddMember("term", rapidjson::Value(term.c_str(), allocator), allocator);
+
+  std::string response = call_esenin_server(
+    "http://" + this->ip + "/nlp/tm/topics",
+    this->port, 
+    doc
+  );
+
+  rapidjson::Document result_doc;
+
+  result_doc.Parse(response.c_str());
+  const rapidjson::Value& value_topics = result_doc["topics"];
+
+  std::vector<double> topics;
+  topics.reserve(value_topics.Size());
+  for (rapidjson::SizeType i = 0; i < value_topics.Size(); i++) {
+    topics.emplace_back(value_topics[i].GetDouble());
+  }
+
+  return topics;
 }
